712e493b04af540487ae938a73b0e95814d6e676
ttm-SP <s.prabhudev@ttmrail.com.au>
2026-04-30 09:29:11 +1000

    CHECKPOINT - Intermidate commit.

--- cmake/toolchains/arm-none-eabi-gcc.cmake
+++ cmake/toolchains/arm-none-eabi-gcc.cmake
@@ -16,24 +16,3 @@ set(CMAKE_SIZE    ${TOOLCHAIN_PREFIX}-size)
 
 # For bare-metal, CMake shouldn't try to run executables during "try_compile"
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
-
-# Optional: produce .elf
-set(CMAKE_EXECUTABLE_SUFFIX ".elf")
-
-# --- CPU settings (override from command line if you want) ---
-# Example defaults: Cortex-M4F hard-float (STM32F4 style)
-set(ARM_CPU "cortex-m4" CACHE STRING "ARM CPU (e.g. cortex-m0, m3, m4, m7, r4f)")
-set(ARM_FPU "fpv4-sp-d16" CACHE STRING "ARM FPU (e.g. fpv4-sp-d16, fpv5-sp-d16, none)")
-set(ARM_FLOAT_ABI "hard" CACHE STRING "float ABI: soft, softfp, hard")
-
-set(ARM_COMMON_FLAGS "-mcpu=${ARM_CPU} -mthumb")
-
-if(NOT ARM_FPU STREQUAL "none")
-  set(ARM_COMMON_FLAGS "${ARM_COMMON_FLAGS} -mfpu=${ARM_FPU} -mfloat-abi=${ARM_FLOAT_ABI}")
-endif()
-
-# --- Common embedded flags (you can tune these later) ---
-set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS}   ${ARM_COMMON_FLAGS} -ffreestanding -fno-builtin")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ARM_COMMON_FLAGS} -ffreestanding -fno-builtin -fno-exceptions -fno-rtti")
-
-# Linker flags are usually set per-target because they depend on your linker script
